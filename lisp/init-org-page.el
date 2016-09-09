e9705f4a0ef104e4d839271e14f5453a4231b131
zhcosin <zhcosin@gmail.com>
2016-09-09 17:24:09 +0800

    modify the path for repository.

--- lisp/init-org-page.el
+++ lisp/init-org-page.el
@@ -3,7 +3,7 @@
 (setq op/site-main-title "长春阁")
 (setq op/site-sub-title "无才可去补苍天，枉入红尘若许年")
 (setq op/personal-github-link "http://zhcosin.github.io")
-(setq op/repository-directory "F:/zhcosin.github.io")
+(setq op/repository-directory "~/zhcosin.github.io")
 ;;(setq op/repository-directory "git@github.com:zhcosin/zhcosin.github.io.git")
 ;;(setq op/repository-directory "https://github.com/zhcosin/zhcosin.github.io.git")
 (setq op/site-domain "http://zhcosin.github.io")
