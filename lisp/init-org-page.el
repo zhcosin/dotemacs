8b9d321ed32868bef350c5a0e749aca5b0335b8d
zhcosin <zhcosin@163.com>
2016-10-14 17:33:41 +0800

    update configure for org-page.

--- lisp/init-org-page.el
+++ lisp/init-org-page.el
@@ -1,9 +1,10 @@
 
 (require 'org-page)
-(setq op/site-main-title "长春阁")
-(setq op/site-sub-title "无才可去补苍天，枉入红尘若许年")
-(setq op/personal-github-link "http://zhcosin.github.io")
+(setq op/theme 'mdo)
+(setq op/site-main-title "金榜除名")
+(setq op/site-sub-title "七八个星天外，两三点雨山前。")
 (setq op/repository-directory "~/zhcosin.github.io")
+(setq op/personal-github-link "https://github.com/zhcosin")
 ;;(setq op/repository-directory "git@github.com:zhcosin/zhcosin.github.io.git")
 ;;(setq op/repository-directory "https://github.com/zhcosin/zhcosin.github.io.git")
 (setq op/site-domain "http://zhcosin.github.io")
