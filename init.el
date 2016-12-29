1f9f7df856d1937ecf83394880a2b9180ba79fd8
zhcosin <zhcosin@gmail.com>
2016-12-29 10:31:33 +0800

    Add plugin which-key.

--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 ;; note: 配置修改后生效
 ;; 在每次编辑配置文件后，刚刚做的修改并不会立刻生效。这时你需要重启编辑器或者重新加载配置文件。重新加载配置文件你需要在当前配置文件中使用 M-x load-file 双击两次回车确认默认文件名，或者使用 M-x eval-buffer 去执行当前缓冲区的所有 Lisp 命令。你也可以使用 C-x C-e 来执行某一行的 Lisp 代码。这些可使刚刚修改的配置文件生效。当然你也可以将这些函数绑定为快捷键。
 
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (defconst cosin/befor-init-time (current-time))
 
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
@@ -16,6 +23,7 @@
 (require 'init-window-number)
 (require 'init-orgmode)
 (require 'init-company)
+(require 'init-which-key)
 ;;(require 'init-smex)
 (require 'init-helm)
 (require 'init-tramp)
@@ -38,3 +46,18 @@
 (defconst cosin/after-init-time (current-time))
 
 (message "This start spent %.2f seconds." (float-time (subtract-time cosin/after-init-time cosin/befor-init-time)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (which-key helm smex desktop session window-number popwin monokai-theme company evil magit auctex company-auctex org-page flycheck chinese-number parinfer helm-swoop swiper-helm)))
+ '(session-use-package t nil (session)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
---
+++ lisp/init-which-key.el
@@ -0,0 +1,8 @@
+
+(zhcosin/install-package-if-not-found 'which-key)
+
+(require 'which-key)
+
+(which-key-mode 1)
+
+(provide 'init-which-key)
