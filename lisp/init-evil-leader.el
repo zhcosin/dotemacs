d69ec9813787b4ab2201c713eead18dd9e8dacbc
zhcosin <zhcosin@gmail.com>
2017-01-06 13:51:17 +0800

    adjust key bindings make it like spacemacs.

--- lisp/init-evil-leader.el
+++ lisp/init-evil-leader.el
@@ -7,7 +7,25 @@
 (evil-leader/set-leader "<SPC>")
 
 (evil-leader/set-key
+  "<SPC>" 'helm-M-x
+  "0" 'select-window-0
+  "1" 'select-window-1
+  "2" 'select-window-2
+  "3" 'select-window-3
+  "4" 'select-window-4
+  "5" 'select-window-5
+  "6" 'select-window-6
+  "7" 'select-window-7
+  "8" 'select-window-8
+  "9" 'select-window-9
+  "qq" 'save-buffers-kill-terminal
   "ff" 'helm-find-files
-  "fs" 'save-buffer)
+  "fs" 'save-buffer
+  "bb" 'switch-to-buffer
+  "bk" 'kill-buffer
+  "bl" 'electric-buffer-list
+  "hdf" 'describe-function
+  "hdv" 'describe-variable
+  "hdk" 'describe-key)
 
 (provide 'init-evil-leader)
