6689c3e9b54aff6611e67c0d91f8fd9c40313db5
zhcosin <zhcosin@gmail.com>
2017-02-15 21:56:49 +0800

    enable cdlatex to LaTeX-mode-hook

--- lisp/init-cdlatex.el
+++ lisp/init-cdlatex.el
@@ -3,7 +3,8 @@
 
 (use-package cdlatex
   :config
-  (add-hook 'org-mode-hook 'turn-on-org-cdlatex))
+  (add-hook 'org-mode-hook 'turn-on-org-cdlatex)
+  (add-hook 'LaTeX-mode-hook 'turn-on-cdlatex))
   
 
 (provide 'init-cdlatex)
