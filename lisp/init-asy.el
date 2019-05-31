a1ad5b8e12660556179cd89ae3bcc64a014adfe0
zhcosin <zhcosin@163.com>
2019-05-31 21:42:06 +0800

    asy-mode 文件的路径按操作系统作区分处理.

--- lisp/init-asy.el
+++ lisp/init-asy.el
@@ -1,5 +1,7 @@
+;添加 asy-mode.el 文件所在目录，路径按操作系统区分.
+(cond ((eq system-type 'windows-nt)(add-to-list 'load-path "E:\\texlive\\2015\\texmf-dist\\asymptote"))
+      ((eq system-type 'gnu/linux)(add-to-list 'load-path "/usr/local/texlive/2017/texmf-dist/asymptote")))
 
-(add-to-list 'load-path "E:\\texlive\\2015\\texmf-dist\\asymptote")
 (autoload 'asy-mode "asy-mode.el" "Asymptote major mode." t)
 (autoload 'lasy-mode "asy-mode.el" "hybrid Asymptote/Latex major mode." t)
 (autoload 'asy-insinuate-latex "asy-mode.el" "Asymptote insinuate LaTeX." t)
