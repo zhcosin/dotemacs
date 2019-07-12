200400771fd79b00ce402faeccaa5f6a9271b625
zbiao <zhcosin@163.com>
2019-07-12 16:32:43 +0800

    python环境添加 flycheck.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -1,6 +1,12 @@
 
 (zhcosin/install-package-if-not-found 'elpy)
+(zhcosin/install-package-if-not-found 'flycheck)
+(zhcosin/install-package-if-not-found 'material-theme)
 
 (elpy-enable)
+
+(when (require 'flycheck nil t)
+  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
+  (add-hook 'elpy-mode-hook 'flycheck-mode))
 
 (provide 'init-python)
