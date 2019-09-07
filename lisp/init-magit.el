e68cfc0059e0bb6f23c938940592758166e36c94
zbiao <zhcosin@163.com>
2019-09-07 20:40:20 +0800

    为了解决magit在进行文件内容部分暂存提交时因设置标记按键C-SPC被输入法捕 获，而将设置标记的按键改为M-SPC.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,4 +1,8 @@
 
 (zhcosin/install-package-if-not-found 'magit)
 
+;; 在对文件修改内容进行部分暂存/提交时，设置标记按键 C-SPC 会被输入法捕获，因此将设置标记绑定到 M-SPC 按键.
+(global-unset-key (kbd "C-SPC"))
+(global-set-key (kbd "M-SPC") 'set-mark-command)
+
 (provide 'init-magit)
