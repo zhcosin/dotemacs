5e3807197979f2809ee0cc94cc9cc327f0dd924e
zbiao <zhcosin@163.com>
2019-09-07 20:42:38 +0800

    将magit-status绑定到按键C-x g.

--- lisp/init-magit.el
+++ lisp/init-magit.el
@@ -1,6 +1,9 @@
 
 (zhcosin/install-package-if-not-found 'magit)
 
+;; 绑定git仓库目录状态命令
+(global-set-key (kbd "C-x g") 'magit-status)
+
 ;; 在对文件修改内容进行部分暂存/提交时，设置标记按键 C-SPC 会被输入法捕获，因此将设置标记绑定到 M-SPC 按键.
 (global-unset-key (kbd "C-SPC"))
 (global-set-key (kbd "M-SPC") 'set-mark-command)
