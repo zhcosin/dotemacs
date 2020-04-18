789f9926beca4c4eb92c0920536c62e82233283b
zbiao <zhcosin@163.com>
2020-04-18 23:13:58 +0800

    correct the configure for company.

--- lisp/init-company.el
+++ lisp/init-company.el
@@ -1,8 +1,6 @@
 (zhcosin/install-package-if-not-found 'company)
 
-(use-package company-mode
-  :config
-  ;; 开启全局 Company 补全
-  (global-company-mode t))
+;; 开启全局 Company 补全
+(global-company-mode t)
 
 (provide 'init-company)
