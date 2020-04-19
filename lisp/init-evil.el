2f24db33f1c08caa35390b2a207138178afce7bc
zbiao <zhcosin@163.com>
2020-04-19 17:51:46 +0800

    add macro to define evil text object and use it define identify for program language.

--- lisp/init-evil.el
+++ lisp/init-evil.el
@@ -11,17 +11,25 @@
   :config
   (setq evil-insert-state-map (make-sparse-keymap))
   (define-key evil-insert-state-map (kbd "<escape>") 'evil-normal-state)
-  (evil-mode t))
-;;(setq-default evil-auto-indent t)
-;;(setq evil-shift-width 4)
-;;(setq evil-repeat-move-cursor t) 
-;;(setq evil-find-skip-newlines nil)
-;;(setq evil-move-cursor-back t)
-;;(setq evil-want-fine-undo nil)
-;;(setq evil-regexp-search t)
-;;(setq evil-search-wrap t)
-;;(setq evil-flash-delay 3)
-;;(setq evil-want-C-i-jump nil)
-;;(setq evil-want-C-u-scrollnil)
+  (evil-mode t)
+
+  ;; macro for define new text object and bind to key in visual mode,
+  ;; if key is 'i', then press vii to select the text object at point.
+  ;; code from: https://stackoverflow.com/questions/18102004/emacs-evil-mode-how-to-create-a-new-text-object-to-select-words-with-any-non-sp
+  ;; thanks.
+  (defmacro define-and-bind-text-object (key start-regex end-regex)
+    (let ((inner-name (make-symbol "inner-name"))
+          (outer-name (make-symbol "outer-name")))
+      `(progn
+         (evil-define-text-object ,inner-name (count &optional beg end type)
+           (evil-select-paren ,start-regex ,end-regex beg end type count nil))
+         (evil-define-text-object ,outer-name (count &optional beg end type)
+           (evil-select-paren ,start-regex ,end-regex beg end type count t))
+         (define-key evil-inner-text-objects-map ,key (quote ,inner-name))
+         (define-key evil-outer-text-objects-map ,key (quote ,outer-name)))))
+
+  ;; define text object for identify for program language,
+  ;; press key vii to select the identify at point.
+  (define-and-bind-text-object "i" "[^a-zA-Z0-9-_]" "[^a-zA-Z0-9-_]"))
 
 (provide 'init-evil)
